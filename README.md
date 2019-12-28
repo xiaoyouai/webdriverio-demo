bdabaf257c5efa35cecfbac8f209290110408ca1
lysh <lysh>
2019-12-28 10:45:32 +0800

    chore: modify some setting

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Run `npm run test or npx wido --spec XXX` to see help information.
 
 `npm run test`
 `npx wdio --spec test/test1.js`
+
+
+<!-- 设置代理：set http_proxy=http://127.0.0.1:8080 -->
--- wdio.conf.js
+++ wdio.conf.js
@@ -5,7 +5,7 @@ exports.config = {
     maxInstances: 1, //最大的并发度运行测试个数，set maxInstance for all browser
     capabilities: [{
         maxInstances: 5, // 最多启动多少个浏览器窗口并行执行
-        browserName: "chrome" // options: chrome || firefox || MicrosoftEdge || safari
+        browserName: "chrome" // options: chrome || safari
             //(ps:use safari just need set Develop->Allow Remote Automation,when use safari,
             //you need avoid interactions with the browser that affect automated testing)
     }],
@@ -19,12 +19,6 @@ exports.config = {
             outputDir: "allure-results"
         }
     },
-    // services: ["selenium-standalone"],
-    // seleniumArgs: {
-    //   javaArgs: [
-    //     "-Dwebdriver.edge.driver=C:\\Windows\\System32\\MicrosoftWebDriver.exe"
-    //   ] //指定使用32位的驱动，用于修复edge浏览器不能正常使用的问题
-    // }, //Selenium服务器的参数映射，直接传递给Selenium.start（）
     framework: "mocha", //采用什么测试框架
     mochaOpts: {
         ui: "bdd", //
